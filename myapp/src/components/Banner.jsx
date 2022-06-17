316db21b0b2389b3569ea10f836888c2f20db061
Bharani619 <bharanish619@gmail.com>
2022-06-17 12:45:09 +0530

    resolved

--- myapp/src/components/Banner.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Banner = () => {
-  return (
-    <div>
-        <img src="https://tpc.googlesyndication.com/simgad/12545784389913602894" alt="" />
-    </div>
-  )
-}
